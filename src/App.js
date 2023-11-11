355a1af505ba4698fdac7ba69b9ff187d5b7dcbd
Makarand1999 <kshirsagarmakarand1999@gmail.com>
2023-11-12 03:16:40 +0530

    Added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DoctersView from './DoctersView/DoctersView';
+
+function App() {
+  return (
+    <div className="App">
+    <DoctersView/>
+    </div>
+  );
+}
+
+export default App;
